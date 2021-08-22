d401abc0b797e4ca1b9f6df3f50c08dbb64d04bb
shilu <dayeonkim5831@gmail.com>
2021-08-22 16:11:26 +0900

    For Jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
+    `maven-publish`
     kotlin("jvm") version "1.5.21"
 }
 
@@ -7,6 +10,7 @@ version = "1.0.0"
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io/")
 }
 
 dependencies {
@@ -14,4 +18,41 @@ dependencies {
     implementation("io.ktor:ktor-client-core:1.6.2")
     implementation("io.ktor:ktor-client-cio:1.6.2")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.12.4")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+}
+
+val javadoc: Javadoc by tasks
+
+val javadocJar = task<Jar>("javadocJar") {
+    from(javadoc.destinationDir)
+    archiveClassifier.set("javadoc")
+    dependsOn(javadoc)
+}
+
+val sourcesJar = task<Jar>("sourcesJar") {
+    from(sourceSets["main"].allSource)
+    archiveClassifier.set("sources")
+}
+
+tasks {
+    build {
+        dependsOn(javadocJar)
+        dependsOn(sourcesJar)
+        dependsOn(jar)
+    }
+}
+
+publishing.publications {
+    register("Release", MavenPublication::class) {
+        from(components["java"])
+        groupId = project.group as String
+        artifactId = project.name
+        version = project.version as String
+
+        artifact(javadocJar)
+        artifact(sourcesJar)
+    }
 }
